47c8411d8a518663dc13701b2528f4165e138326
kibaiar <aidar1996@gmail.com>
2024-07-12 19:19:50 +0300

    Added README

--- app/utils/polygon.py
+++ app/utils/polygon.py
@@ -1,9 +1,5 @@
 import asyncio
-import json
-
 from web3 import Web3
-from web3.exceptions import BadFunctionCallOutput
-
 from app.core.config import settings
 
 WEB3_PROVIDER_URI = settings.WEB3_PROVIDER_URI
@@ -25,4 +21,28 @@ async def get_balance(address: str) -> str:
     checksum_address = web3.to_checksum_address(address)
     balance = await asyncio.to_thread(token_contract.functions.balanceOf(checksum_address).call)
     return balance
+
+
+
+async def get_all_transfer_events():
+    try:
+        transfer_filter = await asyncio.to_thread(token_contract.events.Transfer.createFilter, {'fromBlock': 0})
+        events = await asyncio.to_thread(transfer_filter.get_all_entries)
+        return events
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+
+def get_top_holders(events, top_n):
+    balances = {}
+    for event in events:
+        from_address = event['args']['from']
+        to_address = event['args']['to']
+        value = event['args']['value']
+        if from_address != '0x0000000000000000000000000000000000000000':
+            balances[from_address] = balances.get(from_address, 0) - value
+        balances[to_address] = balances.get(to_address, 0) + value
+
+    top_holders = sorted(balances.items(), key=lambda item: item[1], reverse=True)[:top_n]
+    return top_holders
 
